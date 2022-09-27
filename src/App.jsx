f87bbd3e28e79f0195ebe84970d62a24b69a32e7
MayconCord <mcc@discente.ifpe.edu.br>
2022-09-26 22:24:49 -0300

    consumindo dados da API

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,15 @@
 import React, {useEffect, useState} from "react";
-//import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from "./components/Navbar/Navbar";
-import Search from "./components/Search/Search";
 import Pokedex from "./components/Pokedex/Pokedex";
 
 function App() {
 
-  const [page, setPage] = useState(0);
-  const [totalPages, setTotalPages] = useState(0);
   const [loading, setLoading] = useState(false);
   const [pokemons, setPokemons] = useState([]);
-  const itensPerPage = 27;
 
-  const getPokemons = async (limit = 50, offset) => {
+  const getPokemons = async (limit = 200, offset = 0) => {
     try {
-        let url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}`;
+        let url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}$offset=${offset}`;
         const response = await fetch(url)
         return await response.json()
     } catch (error){
@@ -34,7 +29,7 @@ function App() {
   const fetchPokemons = async () => {
     try {
       setLoading(true);
-      const data = await getPokemons(itensPerPage, itensPerPage*page);
+      const data = await getPokemons();
       const promisses = data.results.map(async (pokemon) => {
         return await getPokemonData(pokemon.url);
       })
@@ -42,7 +37,6 @@ function App() {
       const results = await Promise.all(promisses)
       setPokemons(results);
       setLoading(false);
-      setTotalPages(Math.ceil(data.count / itensPerPage));
     } catch (error) { 
       console.log("fetchPokemons error: ", error)
     }
@@ -50,7 +44,7 @@ function App() {
 
   useEffect(() => {
     fetchPokemons();
-  }, [page])
+  }, [])
 
   return (
     <main>
@@ -58,9 +52,6 @@ function App() {
         <Navbar />
         <Pokedex
           loading={loading} 
-          page={page} 
-          setPage={setPage}
-          totalPages={totalPages} 
           pokemons={pokemons} 
         />
       </div>    
